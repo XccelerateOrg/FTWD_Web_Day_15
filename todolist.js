57c4fccc866a81ec275ea2d0e648a50314507160
mtthwleung <mtthwleung@gmail.com>
2023-06-29 23:47:58 +0800

    done

--- todolist.js
+++ todolist.js
@@ -26,3 +26,53 @@
  * List
  * ==================================
  ***********************************************/
+
+const addItem = document.forms['add-item'];
+
+addItem.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newItem = addItem.querySelector('input[type="text"]').value;
+
+    const ul = document.querySelector("#todo");
+    const list = document.createElement("li");
+    const itemName = document.createElement("span");
+    const deleteButton = document.createElement("span");
+
+    deleteButton.textContent = 'X';
+    itemName.textContent = newItem;
+
+    itemName.classList.add('name');
+    deleteButton.classList.add('delete');
+
+    ul.appendChild(list);
+    list.appendChild(itemName);
+    list.appendChild(deleteButton);
+
+    addItem.querySelector('input[type="text"]').value = "";
+
+    deleteButton.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+    })
+
+    deleteButton.addEventListener("mouseleave", (e) => {
+        e.target.style.backgroundColor = "";
+    });
+
+})
+
+const toDo = document.querySelector("#todo")
+
+toDo.addEventListener("click", (e) => {
+    if (e.target.className === 'delete') {
+        const li = e.target.parentElement;
+        toDo.removeChild(li);
+    }
+})
+
+// let deleteBtns = document.querySelectorAll(".delete");
+
+// deleteBtns.forEach((button) => {
+//     button.addEventListener("mouseover", (e) => {
+//         e.target.style.backgroundColor = "green";
+//     })
+// })
